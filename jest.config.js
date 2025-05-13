2ab813081bd1a1f4793068fc21cae292f14a0d14
Anísio Oliveira <anisioalbuquerque71@gmail.com>
2025-05-13 17:10:12 -0300

    v1.2

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
-  jestEnvironment: 'jsdom',
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   coverageDirectory: 'coverage',
   collectCoverage: true,
--- tests/menu.test.js
+++ tests/menu.test.js
@@ -2,7 +2,9 @@ describe('Menu Management', () => {
   test('should add a new menu item', () => {
     const menu = [];
     menu.push({ name: 'Sushi', price: 10 });
-    expect(menu).toHaveLength(1);
+    menu.push({ name: 'Sashimi', price: 20 });
+    expect(menu).toHaveLength(2);
     expect(menu[0].name).toBe('Sushi');
+    expect(menu[1].name).toBe('Sashimi');
   });
 });
