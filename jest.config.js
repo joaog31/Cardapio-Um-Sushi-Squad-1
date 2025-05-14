5f3e5373a5c4a1872f47a3166daa3bed95d9235b
Breno-Viana <150757313+Breno-Viana@users.noreply.github.com>
2025-05-14 19:17:45 -0300

    att

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-  coverageDirectory: 'coverage',
-  collectCoverage: true,
-  coverageReporters: ['html', 'text', 'lcov'],
-};
