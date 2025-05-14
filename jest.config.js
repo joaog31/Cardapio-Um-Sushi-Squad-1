2e41c100f043dc30aa7cbe1e6dc883901818ddc8
Anísio Oliveira <anisioalbuquerque71@gmail.com>
2025-05-14 11:00:30 -0300

    Git rm

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-  transform: { '^.+\\.[jt]s$': 'ts-jest', },
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['html', 'text', 'lcov'],
-};
