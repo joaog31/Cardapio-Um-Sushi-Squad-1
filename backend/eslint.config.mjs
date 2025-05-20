f842e252bcb33a5425e1b4837c710399f322d2a7
Anísio Oliveira <anisioalbuquerque71@gmail.com>
2025-05-20 20:27:38 -0300

    eslint.config back and front setup

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -32,6 +32,12 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@next/next/no-html-link-for-pages': 'off',
+      'no-console': 'warn',
+      eqeqeq: 'error',
+      curly: 'error',
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn'
--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
+import request from 'supertest'; // ✅ Correta importação
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: INestApplication; // ✅ Sem generic
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -16,7 +16,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(app.getHttpServer()) // ✅ Correto uso do supertest
       .get('/')
       .expect(200)
       .expect('Hello World!');
