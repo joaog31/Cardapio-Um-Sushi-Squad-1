beef474d3613d0474e6b36db40f9503f570a3876
Anísio Oliveira <anisioalbuquerque71@gmail.com>
2025-05-20 21:58:28 -0300

    setup of next plugin for build

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -26,6 +26,10 @@ export default [
       react: eslintPluginReact,
       "react-hooks": eslintPluginReactHooks,
     },
+    extends: [
+      "next",
+      "next/core-web-vitals",
+    ],
     rules: {
       "react/react-in-jsx-scope": "off",
       "react-hooks/rules-of-hooks": "error",
