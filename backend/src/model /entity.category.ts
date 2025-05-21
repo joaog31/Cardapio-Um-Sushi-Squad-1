ed511dd0087ecea9775471797fb9e70c3a888363
Breno-Viana <150757313+Breno-Viana@users.noreply.github.com>
2025-05-21 17:31:47 -0300

    feature: creating entity category

---
+++ backend/src/model /entity.category.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { v4 as uid } from 'uuid';
+
+@Entity()
+export class Category {
+    @PrimaryGeneratedColumn()
+    private id: string;
+
+    @Column()
+    private name: string;
+
+    @Column()
+    private status: CategoryStatus;
+
+    constructor(name: string, status: CategoryStatus) {
+        this.id = uid();
+        this.name = name;
+        this.status = status;
+    }
+}
+
+enum CategoryStatus {
+    AVAILABLE = 'AVAILABLE',
+    UNAVAILABLE = 'UNAVAILABLE',
+}
