17ddf315a0d9f7d559bd13a708856571d6d5e23e
Anísio Oliveira <anisioalbuquerque71@gmail.com>
2025-05-20 22:04:30 -0300

    changes in next plugin

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -26,10 +26,6 @@ export default [
       react: eslintPluginReact,
       "react-hooks": eslintPluginReactHooks,
     },
-    extends: [
-      "next",
-      "next/core-web-vitals",
-    ],
     rules: {
       "react/react-in-jsx-scope": "off",
       "react-hooks/rules-of-hooks": "error",
