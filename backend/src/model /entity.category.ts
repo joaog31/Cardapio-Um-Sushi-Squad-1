01ed7cbca8742071318c1d2872ab536701660e7a
Breno-Viana <150757313+Breno-Viana@users.noreply.github.com>
2025-05-21 18:04:18 -0300

    fix: ident fix error

--- backend/src/model /entity.category.ts
+++ backend/src/model /entity.category.ts
@@ -3,23 +3,23 @@ import { v4 as uid } from 'uuid';
 
 @Entity()
 export class Category {
-    @PrimaryGeneratedColumn()
-    private id: string;
+  @PrimaryGeneratedColumn()
+  private id: string;
 
-    @Column()
-    private name: string;
+  @Column()
+  private name: string;
 
-    @Column()
-    private status: CategoryStatus;
+  @Column()
+  private status: CategoryStatus;
 
-    constructor(name: string, status: CategoryStatus) {
-        this.id = uid();
-        this.name = name;
-        this.status = status;
-    }
+  constructor(name: string, status: CategoryStatus) {
+    this.id = uid();
+    this.name = name;
+    this.status = status;
+  }
 }
 
 enum CategoryStatus {
-    AVAILABLE = 'AVAILABLE',
-    UNAVAILABLE = 'UNAVAILABLE',
+  AVAILABLE = 'AVAILABLE',
+  UNAVAILABLE = 'UNAVAILABLE',
 }
